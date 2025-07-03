c895a120b72f807e79fbb2283fcb1a484ac94b0e
Vanshika-sd <vanshika0101s@gmail.com>
2025-07-03 11:42:25 +0530

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,8 +1,8 @@
 <?php
 $host = "sql12.freesqldatabase.com";  // Replace with actual host from dashboard
-$user = "sql12345678";                // Your DB username
-$pass = "yourpassword";               // Your DB password
-$db   = "sql12345678";                // Your DB name
+$user = "sql12788048";                // Your DB username
+$pass = "5tcu679AWt";               // Your DB password
+$db   = "sql12788048";                // Your DB name
 
 $conn = new mysqli($host, $user, $pass, $db);
 
